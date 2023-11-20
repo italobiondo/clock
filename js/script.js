18afdc7621d72d8e70c34418fcdd021de97f8df1
italo.biondo <italo.a.biondo@gmail.com>
2023-11-19 21:46:27 -0300

    Finalizado

--- js/script.js
+++ js/script.js
@@ -12,6 +12,9 @@ const getTime = () => {
 };
 
 setInterval(() => {
-  
+  const { seconds, minutes, hours } = getTime();
+  secondHand.style.transform = `translate(0,-50%) rotate(${seconds * 6}deg)`;
+  minuteHand.style.transform = `translate(0,-50%) rotate(${minutes * 6}deg)`;
+  hourHand.style.transform = `translate(0,-50%) rotate(${hours * 30}deg)`;
 }, 1000);
 
